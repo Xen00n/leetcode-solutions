b8e024f9ea8c6362785e5a8d8761dee050fa5f72
Xen00n <nikunj11pokharel@gmail.com>
2025-09-26 18:37:18 +0545

    611) Valid Triangle Number

---
+++ 611.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int ans = 0;
+        int n = nums.size();
+        for(int k = n-1;k>=2;k--){
+            int i = 0,j=k-1;
+            while(i<j){
+                if(nums[i]+nums[j]>nums[k]){
+                    ans+=(j-i);
+                    j--;
+                }
+                else{
+                    i++;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+int main(){
+  Solution sol;
+  vector<int>inpp{1,2,3,4,5,6};
+  cout<<sol.triangleNumber(inpp);
+  return 0;
+}
