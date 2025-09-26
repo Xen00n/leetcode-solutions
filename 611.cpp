ecc4dea5c6381bd5d20c61253d0354617c4f3239
Xen00n <nikunj11pokharel@gmail.com>
2025-09-26 18:38:34 +0545

    611) Valid Triangle Number

--- 611.cpp
+++ 611.cpp
@@ -23,7 +23,30 @@ public:
         return ans;
     }
 };
+/*class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int ans = 0;
+        int n = nums.size();
+        for(int k = n-1;k>=2;k--){
+            int i = 0,j=k-1;
+            while(i<j){
+                if(nums[i]+nums[j]>nums[k]){
+                    ans+=(j-i);
+                    j--;
+                }
+                else{
+                    i++;
+                }
+            }
+        }
+        return ans;
+    }
+};
 
+This was my first solution which was not too optimum.... Nice doing this too <3
+*/
 int main(){
   Solution sol;
   vector<int>inpp{1,2,3,4,5,6};
