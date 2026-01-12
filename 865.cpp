058a5ed33dd51cdae9c128d9eb06b52c04dabcd6
Xen00n <nikunj11pokharel@gmail.com>
2026-01-12 17:14:03 +0545

    Smallest Subtree with all Deepest Nodes

---
+++ 865.cpp
@@ -0,0 +1,41 @@
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int height(TreeNode * root){
+        if(root == NULL){
+            return 0;
+        }
+        else{
+            return 1 + max(height(root->left),height(root->right));
+        }
+    }
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        int l = height(root->left);
+        int r = height(root->right);
+        if(l==r){
+            return root;
+        }
+        else if(l>r){
+            return subtreeWithAllDeepest(root->left);           
+        }
+        else{
+            return subtreeWithAllDeepest(root->right);
+        }
+    }
+};
+
