85cd2a74bc957b7cb3c47b0243962bc9fa226b56
Xen00n <nikunj11pokharel@gmail.com>
2026-02-24 10:43:53 +0545

    129) Sum Root to Leaf Numbers

---
+++ 129.cpp
@@ -0,0 +1,22 @@
+
+
+class Solution {
+public:
+    int helper(TreeNode* root, int val){
+        int right = 0,left=0;
+        if(root->right == NULL && root->left == NULL){
+            return val*10 + root->val;
+        }
+        if(root->right){
+            right = helper(root->right,val*10 + root->val);
+        }
+        if(root->left){
+            left = helper(root->left,val*10 + root->val);
+        }
+        // cout<<right<<" "<<left;
+        return right+left;
+    }
+    int sumNumbers(TreeNode* root) {
+        return helper(root,0);
+    }
+};
