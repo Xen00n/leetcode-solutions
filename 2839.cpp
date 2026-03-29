1ce9b536dc1108b33968403036ad48b57fccd86d
Xen00n <nikunj11pokharel@gmail.com>
2026-03-29 11:23:08 +0545

    2839) Check if Strings Can be Made Equal With Operations I

---
+++ 2839.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        if((s1[0]==s2[0] && s1[2]==s2[2]) || (s1[0]==s2[2] && s1[2]==s2[0])){
+            if((s1[1]==s2[1] && s1[3] == s2[3])||(s1[1]==s2[3]&&s1[3]==s2[1])){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+int main(){
+  Solution sol;
+  cout<<sol.canBeEqual("abcd","cdab");
+  return 0;
+}
